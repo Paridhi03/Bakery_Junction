b2eecd3050d4d2cd785333959a0c22c22013c694
Paridhi03 <98459098+Paridhi03@users.noreply.github.com>
2022-01-26 16:35:40 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppInitializer());
+}
+
+class AppInitializer extends StatelessWidget {
+  const AppInitializer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
